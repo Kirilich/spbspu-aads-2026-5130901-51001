68447911c8d4588136d1f24c496aa87aa9b91f59
liia <kirilich_lubit@rambler.ru>
2026-06-24 22:19:53 +0300

    add push_front and push_back tests

--- nabieva.liia/S1/test-list.cpp
+++ nabieva.liia/S1/test-list.cpp
@@ -25,4 +25,33 @@ BOOST_AUTO_TEST_CASE(test_front_back_throw_on_empty)
     BOOST_CHECK_THROW(list.back(), std::logic_error);
 }
 
+BOOST_AUTO_TEST_CASE(test_push_front)
+{
+    List< int > list;
+
+    list.push_front(10);
+    BOOST_CHECK(!list.empty());
+    BOOST_CHECK_EQUAL(list.front(), 10);
+    BOOST_CHECK_EQUAL(list.back(), 10);
+
+    list.push_front(20);
+    list.push_front(30);
+    BOOST_CHECK_EQUAL(list.front(), 30);
+    BOOST_CHECK_EQUAL(list.back(), 10);
+    BOOST_CHECK_EQUAL(getSize(list), 3u);
+}
+
+BOOST_AUTO_TEST_CASE(test_push_back)
+{
+    List< int > list;
+
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+
+    BOOST_CHECK_EQUAL(list.front(), 1);
+    BOOST_CHECK_EQUAL(list.back(), 3);
+    BOOST_CHECK_EQUAL(getSize(list), 3u);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
